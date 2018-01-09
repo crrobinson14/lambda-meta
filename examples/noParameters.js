fe5bd6873a7e4ea64d36b6b4f2172d77f688ff81
Chad Robinson <crobinson@medialantern.com>
2018-01-09 12:01:37 -0700

    Code cleanup in examples.

--- examples/noParameters.js
+++ examples/noParameters.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: (event, context, callback) => lm.processRequest(module.exports, event, context, callback),
 
     // Here we need no parameters. We simply return a canned value to the caller as our result.
-    handler: () => ({
+    process: () => ({
         environment: process.env.NODE_ENV || 'development',
     }),
 };
--- examples/useParameters.js
+++ examples/useParameters.js
@@ -15,7 +15,7 @@ module.exports = {
 
     entry: (event, context, callback) => lm.processRequest(module.exports, event, context, callback),
 
-    handler: (event, context) => {
+    process: (event, context) => {
         // Note that we can rely on context.params being defined and being an object. And context.params.userId will
         // be defined and be a string.
         console.log('Got a request for user ' + context.params.userId, event);
