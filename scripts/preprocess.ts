f25b1305f81bf489aef1f976a7cd154897c3c4d3
Chad Robinson <crobinson@medialantern.com>
2020-02-17 17:39:01 -0700

    Fix entry point.

--- scripts/preprocess.ts
+++ scripts/preprocess.ts
@@ -14,15 +14,14 @@ export async function processHandlers(subdir: string, prefix: string = '') {
         .map(async file => {
             const handler: any = (await import(file)).default;
 
-            const name = `${prefix}${handler.name || ''}`;
-            functions[name] = {
+            functions[`${prefix}${handler.name || ''}`] = {
                 // In serverless.com definitions, the "handler" is the entry file with the function name as the suffix.
                 // So "myfile.entry" would mean "const handler = require(myfile); handler.entry(...);"
                 handler: path.relative('.', file).replace('.js', '.default.entry').replace('.ts', '.default.entry'),
                 events: handler.events || [],
 
                 // We do this manually because we don't want to copy all the methods, just those useful for docs
-                name,
+                ...(handler.name ? { name: handler.name } : {}),
                 ...(handler.description ? { description: handler.description } : {}),
                 ...(handler.inputs ? { inputs: handler.inputs } : {}),
                 ...(handler.skipResponse ? { skipResponse: handler.skipResponse } : {}),
